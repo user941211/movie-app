76970da41182624a58f50a0b3edfcfdd7b8036bc
user941211 <common941211@gmail.com>
2023-05-20 13:41:43 +0900

    gh-pages broswer router 배포시 오류 해결(mit 라이선스)

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,11 +1,11 @@
 import { Link, useParams } from "react-router-dom";
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback } from "react";
 function Detail(){
     const {movieId} = useParams();
     //console.log(movieId);
     //movieId를 출력할 수 있음
-    const [loading, setLoading] = useState([]);
-    const [movies, setMovies] = useState([]);
+    //const [loading, setLoading] = useState([]);
+    //const [movies, setMovies] = useState([]);
     const getMovie = useCallback(async () => {
         const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`);
         const json = await response.json();
@@ -16,6 +16,11 @@ function Detail(){
     useEffect(() => {
         getMovie();
     }, [getMovie]);
-    return <Link to ="/"><h1>Detail</h1></Link>;
+    return (
+        <div>
+            <h2>콘솔창에 세부정보 있음. 실험용 프로젝트라 귀찮아서 굳이 옮겨놓지는 않음.</h2>;
+            <Link to ="/"><h1>Detail</h1></Link>;
+        </div>
+    );
 }
 export default Detail;
